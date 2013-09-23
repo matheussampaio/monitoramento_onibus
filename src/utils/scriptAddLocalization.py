e078ac2a3417d2c35611ccbfcd914cdcc3476be1
Matheus Sampaio <matheus.sampaio@ccc.ufcg.edu.br>
2013-09-23 12:00:16 -0300

    Adicionado mais duas rotas

--- src/utils/scriptAddLocalization.py
+++ src/utils/scriptAddLocalization.py
@@ -1,11 +1,21 @@
 import psycopg2
 from time import sleep
 
-# listaCoor = range(100)
+arq = open('coordenates500.db', 'r')
 
-arq = open('coordenates.db', 'r')
+listaCoordenadas500 = [line.split(',') for line in arq.read().split('\n')]
 
-listaCoordenadas = [line.split(',') for line in arq.read().split('\n')]
+arq.close()
+
+arq = open('coordenates505.db', 'r')
+
+listaCoordenadas505 = [line.split(',') for line in arq.read().split('\n')]
+
+arq.close()
+
+arq = open('coordenates555.db', 'r')
+
+listaCoordenadas555 = [line.split(',') for line in arq.read().split('\n')]
 
 arq.close()
 
@@ -18,10 +28,28 @@ def main():
 
     cur = conn.cursor()
 
-    for i in range(len(listaCoordenadas)):
-        cur.execute("INSERT INTO Localization VALUES (DEFAULT, 1, {0}, {1}, NOW());".format(listaCoordenadas[i][0], listaCoordenadas[i][1]))
+    i, j, k = 0, 0, 0
+
+    while True:
         
-        print("Localization create: 1, {0}, {1}, NOW());".format(listaCoordenadas[i][0], listaCoordenadas[i][1]))
+        if i > len(listaCoordenadas500):
+            i = 0
+        else:
+            i += 1
+            
+        if j > len(listaCoordenadas505):
+            j = 0
+        else:
+            j += 1
+
+        if k > len(listaCoordenadas555):
+            k = 0
+        else:
+            k += 1
+
+        cur.execute("INSERT INTO Localization VALUES (DEFAULT, (SELECT id_onibus FROM Onibus WHERE placa = 'ABC-0001'), {0}, {1}, NOW());".format(listaCoordenadas500[i][0], listaCoordenadas500[i][1]))
+        cur.execute("INSERT INTO Localization VALUES (DEFAULT, (SELECT id_onibus FROM Onibus WHERE placa = 'ABC-0004'), {0}, {1}, NOW());".format(listaCoordenadas505[j][0], listaCoordenadas505[j][1]))
+        cur.execute("INSERT INTO Localization VALUES (DEFAULT, (SELECT id_onibus FROM Onibus WHERE placa = 'ABC-0007'), {0}, {1}, NOW());".format(listaCoordenadas555[k][0], listaCoordenadas555[k][1]))
 
         sleep(3)
 
