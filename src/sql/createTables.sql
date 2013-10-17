39adb673a484c5986182cacdadb05bdea7f132a0
Matheus Sampaio <matheus1401@gmail.com>
2013-10-17 17:53:52 -0300

    Trigger para atualizar current_seq

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -304,3 +304,22 @@ CREATE OR REPLACE FUNCTION refreshStatusOnibus() RETURNS TRIGGER AS $refreshStat
 
 CREATE TRIGGER refreshStatusOnibus AFTER INSERT ON Localization
     FOR EACH ROW EXECUTE PROCEDURE refreshStatusOnibus();
+
+
+CREATE OR REPLACE FUNCTION refreshCurrentSeq() RETURNS TRIGGER AS $refreshCurrentSeq$
+    DECLARE
+        new_seq INTEGER;
+    BEGIN
+
+        SELECT h.seq INTO new_seq FROM Onibus o, Horario h WHERE o.id_onibus = NEW.id_onibus AND o.id_onibus = h.id_onibus AND (CAST(h.tempo AS TIME) - CAST(NOW() AS TIME)) >= CAST('00:00' AS TIME) ORDER BY (CAST(h.tempo AS TIME) - CAST(NOW() AS TIME)) LIMIT 1;
+
+        IF new_seq IS NOT NULL THEN
+            UPDATE Onibus SET current_seq = new_seq WHERE id_onibus = NEW.id_onibus;
+        END IF;
+
+        RETURN NEW;
+    END;
+    $refreshCurrentSeq$ LANGUAGE plpgsql;
+
+CREATE TRIGGER refreshCurrentSeq AFTER INSERT ON Localization
+    FOR EACH ROW EXECUTE PROCEDURE refreshCurrentSeq();
