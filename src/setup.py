2912c46600995f22b2552f950f57cd082bd32d5f
Matheus Sampaio <matheus1401@gmail.com>
2013-10-29 14:50:44 -0300

    Adicionado tratamento de erro em setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,32 +2,35 @@ import psycopg2
 import os
 import sys
 
-server = 'gonibus'
+server = "gonibus"
 
-if (len(sys.argv) > 1 and sys.argv[1] == 'test'):
-    server = 'teste'
+if (len(sys.argv) > 1 and sys.argv[1] == "test"):
+    server = "teste"
 
 conn = psycopg2.connect("dbname=" + server + " user=postgres")
 conn.set_isolation_level(0) # set autocommit
 cur = conn.cursor()
 
+def execute(command, msg):
+    try:
+        cur.execute(command)
+        print "{0} .. OK".format(msg)
+    except psycopg2.ProgrammingError as err:
+        print "{0} .. FAIL".format(msg), err
+
 def createAll(path, msg):
     for e in os.listdir(path):
-        arq = open(path + '/' + e, 'r')
-        try:
-            cur.execute( arq.read() )
-            print '{0} .. OK'.format(e)
-        except psycopg2.ProgrammingError as err:
-            print '{0} .. FAIL'.format(e), err
+        arq = open(path + "/" + e, "r")
+        execute(arq.read(), e)
         arq.close()
 
-createAll('sql/news/enums', 'CREATE TYPE')
-createAll('sql/news/tables', 'CREATE TABLE')
+createAll("sql/news/enums", "CREATE TYPE")
+createAll("sql/news/tables", "CREATE TABLE")
 
-cur.execute("SELECT AddGeometryColumn('','rota','geom','4291','LINESTRING',2);")
-cur.execute("SELECT AddGeometryColumn('','pontoonibus','geom','4291','POINT',2);")
+execute("SELECT AddGeometryColumn('','rota','geom','4291','LINESTRING',2);", "Rota_LINESTRING")
+execute("SELECT AddGeometryColumn('','pontoonibus','geom','4291','POINT',2);", "PontoOnibus_POINT")
 
-createAll('sql/news/alters', 'ALTER TABLE')
-createAll('sql/news/functions', 'CREATE FUNCTION')
-createAll('sql/news/triggers', 'CREATE TRIGGER')
-createAll('sql/news/views', 'CREATE VIEW')
+createAll("sql/news/alters", "ALTER TABLE")
+createAll("sql/news/functions", "CREATE FUNCTION")
+createAll("sql/news/triggers", "CREATE TRIGGER")
+createAll("sql/news/views", "CREATE VIEW")
