85738257453a967dc7381d1f86f127b66775f566
Matheus Sampaio <matheus1401@gmail.com>
2013-10-29 13:35:20 -0300

    Adicionado placa em LastLocalization

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -56,7 +56,7 @@ CREATE TABLE Localization (
 
 ALTER TABLE Localization ADD FOREIGN KEY (id_onibus) REFERENCES Onibus;
 
-CREATE OR REPLACE VIEW lastLocalization AS SELECT DISTINCT ON (id_onibus) id_onibus, lat AS Latitude, long AS Longitude, ST_GeomFromText('POINT ( ' || lat || ' ' || long || ')', 4291) AS geom FROM Localization ORDER BY id_onibus, time DESC;
+CREATE OR REPLACE VIEW lastLocalization AS SELECT DISTINCT ON (l.id_onibus) l.id_onibus, l.lat AS Latitude, l.long AS Longitude, o.placa, ST_GeomFromText('POINT ( ' || l.lat || ' ' || l.long || ')', 4291) AS geom FROM Localization l, Onibus o WHERE l.id_onibus = o.id_onibus ORDER BY id_onibus, time DESC;
 
 
 CREATE OR REPLACE FUNCTION getIDNextPontoOnibus (id INTEGER) RETURNS INTEGER AS $$
--- src/sql/inserts/views.sql
+++ src/sql/inserts/views.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW rota555view AS SELECT id_rota, nome, ST_GeomFromText(ST_AsText(geom), 4291) AS geom FROM rota WHERE nome = '555';
 CREATE OR REPLACE VIEW rota505view AS SELECT id_rota, nome, ST_GeomFromText(ST_AsText(geom), 4291) AS geom FROM rota WHERE nome = '505';
 CREATE OR REPLACE VIEW rota500view AS SELECT id_rota, nome, ST_GeomFromText(ST_AsText(geom), 4291) AS geom FROM rota WHERE nome = '500';
-CREATE OR REPLACE VIEW lastLocalization AS SELECT DISTINCT ON (id_onibus) id_onibus, lat AS Latitude, long AS Longitude, ST_GeomFromText('POINT ( ' || lat || ' ' || long || ')', 4291) AS geom FROM Localization ORDER BY id_onibus, time DESC;
+CREATE OR REPLACE VIEW lastLocalization AS SELECT DISTINCT ON (l.id_onibus) l.id_onibus, l.lat AS Latitude, l.long AS Longitude, o.placa, ST_GeomFromText('POINT ( ' || l.lat || ' ' || l.long || ')', 4291) AS geom FROM Localization l, Onibus o WHERE l.id_onibus = o.id_onibus ORDER BY id_onibus, time DESC;
